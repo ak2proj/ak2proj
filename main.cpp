573c9ca715d179957357e758059fcd8593a32db1
Robikos <r.a.kostrzewski@gmail.com>
2014-06-03 23:54:15 +0200

    TLB feature

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main()
     std::cout << std::hex;
     
     auto phys = std::make_shared<physical_memory>(4096 * 4096);
-    auto mmu = std::make_shared<amd64_mmu>(phys);
+    auto mmu = std::make_shared<amd64_mmu>(phys, 512);
     
     std::uint8_t program[] = { 
         // setup paging structures 
--- mmu.h
+++ mmu.h
@@ -5,6 +5,7 @@
 #include <unordered_map>
 
 #include "memory.h"
+#include "tlb.h"
 
 class page_fault : public std::runtime_error
 {
@@ -13,7 +14,7 @@ class page_fault : public std::runtime_error
 class mmu
 {
 public:
-    mmu(std::shared_ptr<memory> mem) : _mem{ std::move(mem) }
+    mmu(std::shared_ptr<memory> mem, std::uintmax_t tlb_size) : _mem{ std::move(mem)}, _tlb{ new tlb(tlb_size)}
     {
     }
     
@@ -65,6 +66,7 @@ public:
 protected:
     const std::shared_ptr<memory> _mem;
     std::unordered_map<std::uint64_t, std::uint64_t> _registers;
+    std::unique_ptr<tlb> _tlb;
     bool _enabled = false;
 };
 
@@ -75,76 +77,91 @@ public:
     
     virtual std::uint64_t translate(std::uint64_t address, access_type = data_read()) const override
     {
+    
         if (_enabled)
         {
-            std::cout << "[mmu] attempting to translate 0x" << address << '\n';
-            
-            //the first address from cr3
-            std::uint64_t cr3 = read_register();
-            
-            std::cout << "[mmu] cr3 = 0x" << cr3 << '\n';
-            
-            cr3 &= ~4095ull;
-            std::cout << "[mmu] pml4 is at 0x" << cr3 << '\n';
-
-            std::uint64_t pml4e = (address >> 39) & 511;
-            std::uint64_t pdpte = (address >> 30) & 511;
-            std::uint64_t pde = (address >> 21) & 511;
-            std::uint64_t pte = (address >> 12) & 511;
-
             std::uint64_t rest = address & 4095ull;
-
-            std::cout << "[mmu] physical address is pml4[0x" << pml4e << "][0x" << pdpte << "][0x" << pde << "][0x" << pte << "] | 0x" << rest << '\n';
-
-            std::uint64_t pml4 = cr3;
-            std::uint64_t pdpt;
-            std::uint64_t pd;
-            std::uint64_t pt;
-            std::uint64_t physical_address;
-
-            _mem->read(pml4 + pml4e * 8, pdpt);
-            std::cout << "[mmu] pml4[0x" << pml4e << "] = 0x" << pdpt << '\n';
-            if (!(pdpt & 1))
-            {
-                throw std::runtime_error{ "not present" };
-            }
             
-            pdpt &= ~4095ull;
-            std::cout << "[mmu] pdpt is at 0x" << pdpt << '\n';
-            
-            _mem->read(pdpt + pdpte * 8, pd);
-            std::cout << "[mmu] pdpt[0x" << pdpte << "] = 0x" << pd << '\n';
-            if (!(pd & 1))
+            if (_tlb->in(address))
             {
-                throw std::runtime_error{ "not present" };
+                std::uint64_t result =  _tlb->get_translation(std::uint64_t address);
+                if (!(result & 1))
+                    throw std::runtime_error{"Not present"};
+                return (result & ~4095ull) | rest;
             }
-
-            pd &= ~4095ull;
-            std::cout << "[mmu] pd is at 0x" << pd << '\n';
-            
-            _mem->read(pd + pde * 8, pt);
-            std::cout << "[mmu] pd[0x" << pde << "] = 0x" << pt << '\n';
-            if (!(pt & 1))
+            else
             {
-                throw std::runtime_error{ "not present" };
-            }
-
-            pt &= ~4095ull;
-            std::cout << "[mmu] pt is at 0x" << pt << '\n';
+                std::cout << "[mmu] attempting to translate 0x" << address << '\n';
             
-            _mem->read(pt + pte * 8, physical_address);
-            std::cout << "[mmu] pt[0x" << pte << "] = 0x" << physical_address << '\n';
-            if (!(physical_address & 1))
-            {
-                throw std::runtime_error{ "not present" };
+                //the first address from cr3
+                std::uint64_t cr3 = read_register();
+            
+                std::cout << "[mmu] cr3 = 0x" << cr3 << '\n';
+            
+                cr3 &= ~4095ull;
+                std::cout << "[mmu] pml4 is at 0x" << cr3 << '\n';
+
+                std::uint64_t pml4e = (address >> 39) & 511;
+                std::uint64_t pdpte = (address >> 30) & 511;
+                std::uint64_t pde = (address >> 21) & 511;
+                std::uint64_t pte = (address >> 12) & 511;
+    
+                
+        
+                std::cout << "[mmu] physical address is pml4[0x" << pml4e << "][0x" << pdpte << "][0x" << pde << "][0x" << pte << "] | 0x" << rest << '\n';
+
+                std::uint64_t pml4 = cr3;
+                std::uint64_t pdpt;
+                std::uint64_t pd;
+                std::uint64_t pt;
+                std::uint64_t physical_address;
+
+                _mem->read(pml4 + pml4e * 8, pdpt);
+                std::cout << "[mmu] pml4[0x" << pml4e << "] = 0x" << pdpt << '\n';
+                if (!(pdpt & 1))
+                {
+                    throw std::runtime_error{ "not present" };
+                }
+            
+                pdpt &= ~4095ull;
+                std::cout << "[mmu] pdpt is at 0x" << pdpt << '\n';
+            
+                _mem->read(pdpt + pdpte * 8, pd);
+                std::cout << "[mmu] pdpt[0x" << pdpte << "] = 0x" << pd << '\n';
+                if (!(pd & 1))
+                {
+                    throw std::runtime_error{ "not present" };
+                }
+
+                pd &= ~4095ull;
+                std::cout << "[mmu] pd is at 0x" << pd << '\n';
+            
+                _mem->read(pd + pde * 8, pt);
+                std::cout << "[mmu] pd[0x" << pde << "] = 0x" << pt << '\n';
+                if (!(pt & 1))
+                {
+                    throw std::runtime_error{ "not present" };
+                }
+
+                pt &= ~4095ull;
+                std::cout << "[mmu] pt is at 0x" << pt << '\n';
+            
+                _mem->read(pt + pte * 8, physical_address);
+                std::cout << "[mmu] pt[0x" << pte << "] = 0x" << physical_address << '\n';
+                if (!(physical_address & 1))
+                {
+                    throw std::runtime_error{ "not present" };
+                }
+
+                _tlb->save_translation(address & ~4095ull, physical_address);
+    
+                physical_address &= ~4095ull;
+                physical_address |= rest;
+
+                std::cout << "[mmu] translated 0x" << address << " to 0x" << physical_address << '\n';
+
+                return physical_address;
             }
-
-            physical_address &= ~4095ull;
-            physical_address |= rest;
-
-            std::cout << "[mmu] translated 0x" << address << " to 0x" << physical_address << '\n';
-
-            return physical_address;
         }
         
         else
